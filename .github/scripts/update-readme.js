95dff070352477c037f5773b396dc72476412fe7
Bamz Kittisak Udomsri <76609512+brownyroll@users.noreply.github.com>
2025-08-20 01:29:46 +0700

    feat: @bbwny

--- .github/scripts/update-readme.js
+++ .github/scripts/update-readme.js
@@ -17,7 +17,8 @@ const config = {
   lang: "html,css,js,ts,react,nextjs,nodejs,vue,php,laravel,dotnet,django,tailwind,bootstrap,express,arduino,mysql,sqlite,mongodb,nginx,docker,git,linux,figma,postman,astro,bash,bun,cloudflare,discord,discordjs"
 };
 
-const readmePath = "README.md";
+
+const readmePath = path.join(__dirname, "../../README.md");
 
 // Create progress bar
 function createProgressBar(percentage, length = 25) {
